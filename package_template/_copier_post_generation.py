c34fd6256005796502d5b1ae55dcd257ccf82a3b
Kyle King <KyleKing@users.noreply.github.com>
2024-03-29 06:27:50 -0400

    fix: update copier-post-generation script

--- package_template/_copier_post_generation.py
+++ package_template/_copier_post_generation.py
@@ -1,11 +1,8 @@
 """Post-Generation Script to be run from Copier."""
 
-# FIXME: Update for mdformat-plugin
-
 import json
 import re
 import shutil
-import sys
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -18,16 +15,13 @@ _IS_PROJ = not _re_copier_dir.search(Path(__file__).absolute().as_posix())
 
 @dataclass
 class Config:
-    cname: str
-    doc_dir: str
-    project_description: str
-    package_name_py: str
-    project_name: str
+    package_name_kebab: str
+    plugin_name: str
+    repository_url: str
 
 
 _CONFIG_PATH = Path(__file__).with_suffix('.json')
 _CONFIG = Config(**json.loads(_CONFIG_PATH.read_text()))
-_CONFIG_PATH.unlink()
 
 
 def _log(message: str) -> None:
@@ -38,80 +32,30 @@ def _log(message: str) -> None:
 def cleanup() -> None:
     """Remove files and folders that are no longer used."""
     paths = [
-        Path('.deepsource.toml'),
-        Path('.doit-db.sqlite'),
-        Path('.doit.tmp-py'),
-        Path('.doit.tmp-toml'),
-        Path('.github/codeql-config.yml'),
-        Path('.github/workflows/codeql-analysis.yml'),
-        Path('.github/workflows/codeql-config.yml'),
-        Path('.github/workflows/upgrade-dependencies.yml'),
-        Path('.pyup.yml'),
-        Path('.sourcery.yaml'),
-        Path('appveyor.yml'),
-        Path('dodo.py'),
-        Path('flake8-full.log'),
-        Path('mypy.ini'),
-        Path('requirements.txt'),
-        Path('ruff.toml'),
-        Path(f'{_CONFIG.doc_dir}/docs/_docs.md'),
-        Path(f'{_CONFIG.doc_dir}/docs/CODE_OF_CONDUCT.md'),
-        Path(f'{_CONFIG.doc_dir}/docs/CONTRIBUTING.md'),
-        Path(f'{_CONFIG.doc_dir}/docs/SECURITY.md'),
     ]
     directories = [
-        Path('.logs'),
-        Path('_adr'),
-        Path(f'{_CONFIG.doc_dir}/css'),
     ]
 
     for pth in paths:
         if pth.is_file():
             _log(f'Removing: {pth}')
-            pth.unlink()  # FYI: "missing_ok" was added in 3.8, but this script is ^3.7
+            pth.unlink()
     for dir_pth in directories:
         if dir_pth.is_dir():
             _log(f'Deleting: {dir_pth}')
             shutil.rmtree(dir_pth)
 
 
-def validate_configuration() -> None:
-    copier_text = Path('.copier-answers.yml').read_text()
-    copier_dict = {
-        line.split(':')[0]: line.split(':')[-1].strip()
-        for line in copier_text.split('\n')
-        if ':' in line
-    }
-
-    errors = []
-    extras_value = copier_dict['install_extras']
-    if extras_value == 'None':
-        errors.append(f'install_extras should be an empty string or list of extras, not {extras_value}')
-    py_tuples = {
-        key: re.compile(r'["\']+').sub('', copier_dict[key]).split('.')
-        for key in ('minimum_python', 'minimum_python_short')
-    }
-    python_value = py_tuples['minimum_python'][:2]
-    python_short_value = py_tuples['minimum_python_short']
-    if python_value != python_short_value:
-        errors.append(f'Error in Python versions ({python_value} != {python_short_value})')
-    if errors:
-        print('\n\n')  # noqa: T201
-        print('Please review the errors below and edit the copier answers accordingly')  # noqa: T201
-        print(errors)  # noqa: T201
-        print('\n\n')  # noqa: T201
-        sys.exit(1)
-
-
 def delete_myself() -> None:
     """Delete this file after completing the main script."""
     Path(__file__).unlink()
+    _CONFIG_PATH.unlink()
 
 
 if __name__ == '__main__':
     _log(
         f"""
-The '{_CONFIG.package_name_py}' package has been updated (or created)!
+The 'mdformat_{_CONFIG.plugin_name}' package has been updated (or created)!
 
 1. Review the changes and commit. Merge conflicts may either be '*.rej' files or as inline git diffs
 2. Install dependencies with 'poetry install --sync'
@@ -120,11 +64,10 @@ The '{_CONFIG.package_name_py}' package has been updated (or created)!
 5. If this is a new project, you could create the GitHub repo with:
 
     ```sh
-    gh repo create "{_CONFIG.project_name}" --source=. --remote=origin --push \
-        --description="{_CONFIG.project_description}" --homepage="{_CONFIG.cname}" --public
+    gh repo create "{_CONFIG.package_name_kebab}" --source=. --remote=origin --push \
+        --homepage="{_CONFIG.repository_url}" --public
     ```
 """,
     )
     cleanup()
-    validate_configuration()
     delete_myself()
