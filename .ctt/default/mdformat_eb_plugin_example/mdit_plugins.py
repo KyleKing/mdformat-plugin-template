e18a661cf924fa0cea2016eeb3422811fc0e25f5
Kyle King <KyleKing@users.noreply.github.com>
2025-11-23 21:13:06 -0600

    test: add missing file to at least run the smoke tests

---
+++ .ctt/default/mdformat_eb_plugin_example/mdit_plugins.py
@@ -0,0 +1,9 @@
+"""Markdown-it plugins for mdformat-eb-plugin-example."""
+
+from __future__ import annotations
+
+from markdown_it import MarkdownIt
+
+
+def eb_plugin_example_plugin(mdit: MarkdownIt) -> None:
+    """A minimal markdown-it plugin for eb_plugin_example."""
